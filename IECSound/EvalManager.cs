dceaa95e2d61e445a064e9974f5eb57ca1252760
masaki muto <m.muto0128@gmail.com>
2014-09-09 00:23:38 +0900

    refactor

---
+++ IECSound/EvalManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IECSound
+{
+	class EvalManager : Masa.IECBomb.EvalManagerBase<SynthParam, ParamName>
+	{
+		public static EvalManager Instance { get; private set; }
+
+		public EvalManager()
+			:base()
+		{
+			Instance = this;
+		}
+	}
+}
---
+++ IECSound/ItemPool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IECSound
+{
+	class ItemPool : Masa.IECBomb.ItemPoolBase<SynthParam, ParamName>
+	{
+		public static ItemPool Instance { get; private set; }
+
+		protected override float Eval(SynthParam item)
+		{
+			return EvalManager.Instance.Eval(item);
+		}
+
+		public ItemPool()
+			: base(10)
+		{
+			Instance = this;
+		}
+	}
+}
